288b8c2c68d8b6962d04e7311123cc65e4c2c73e
salema <kevin@alliknow.net>
2011-08-11 14:31:42 +0200

    Make episode date highlight as well

--- source/EpisodeList.js
+++ source/EpisodeList.js
@@ -80,6 +80,8 @@ enyo.kind({
 			if (this.formatter != undefined) this.$.episodePublished.setContent(this.formatter.format(pubDate));
 			else this.$.episodePublished.setContent(episode.pubDate);
 			
+			if (this.selectedIndex == inIndex) this.$.episodePublished.addClass("highlight");
+			
 			return true;
 		}
 	},
