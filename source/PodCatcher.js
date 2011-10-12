b53d388e4bf053ad6c4f7e04d596c0c01ac88d63
salema <kevin@alliknow.net>
2011-10-12 21:53:36 +0200

    nobody seems to like a peekWidth > 0

--- source/PodCatcher.js
+++ source/PodCatcher.js
@@ -41,10 +41,10 @@ enyo.kind({
 		{kind: "SlidingPane", flex: 1, components: [
 			{kind: "Net.Alliknow.PodCatcher.PodcastList", name: "podcastListPane", width: "230px", onPrepareLoad: "prepareLoad",
 					onSelectPodcast: "podcastSelected", onSelectAll: "allPodcastsSelected", onAutoUpdateComplete: "autoDownload"},
-			{kind: "Net.Alliknow.PodCatcher.EpisodeList", name: "episodeListPane", width: "360px", peekWidth: 100,
+			{kind: "Net.Alliknow.PodCatcher.EpisodeList", name: "episodeListPane", width: "380px",
 					onSelectEpisode: "episodeSelected", onPlaylistChanged: "playlistChanged", onSpecialListSelected: "specialListSelected",
 					onResumeComplete: "propagateMarkedEpisodesToPodcastList"},
-			{kind: "Net.Alliknow.PodCatcher.EpisodeView", name: "episodeViewPane", flex: 1, peekWidth: 250, onTogglePlay: "updateDashboard", onNext: "playNext",
+			{kind: "Net.Alliknow.PodCatcher.EpisodeView", name: "episodeViewPane", flex: 1, onTogglePlay: "updateDashboard", onNext: "playNext",
 					onPlaybackEnded: "playNext", onResume: "updateDashboard", onMarkEpisode: "episodeMarked", onOpenInBrowser: "openInBrowser",
 					onDownloaded: "episodeDownloaded", onDeleteDownload: "deleteDownloadedEpisode"}
 		]}
