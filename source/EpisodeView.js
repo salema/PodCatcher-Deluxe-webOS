8e644b45b691f7b858bf75e7134f190bcb8fb575
salema <kevin@alliknow.net>
2011-10-14 20:29:26 +0200

    FEATURE: Make Bluetooth buttons work
    Conflicts:
    	source/EpisodeView.js
    	source/PodCatcher.js

--- source/EpisodeView.js
+++ source/EpisodeView.js
@@ -29,8 +29,8 @@ enyo.kind({
 		onOpenInBrowser: ""
 	},
 	components: [
-		{kind: "PalmService", name: "headsetService", service: "palm://com.palm.keys/headset/", method: "status",
-			subscribe: true, onSuccess: "headsetStatusChanged"},
+		{kind: "PalmService", name: "headsetService", service: "palm://com.palm.keys/headset/", method: "status", onSuccess: "headsetStatusChanged"},
+		{kind: "PalmService", name: "headsetButtonService", service: "palm://com.palm.keys/media/", method: "status", onSuccess: "headsetButtonPressed"},
 		{kind: "Header", layoutKind: "HFlexLayout", className: "header", components: [
 			{name: "episodeName", content: $L("Listen"), className: "nowrap", flex: 1},
 			{kind: "Spinner", name: "stalledSpinner", align: "right"}
@@ -53,7 +53,10 @@ enyo.kind({
 		this.plays = false;
 		this.sliderInterval = setInterval(enyo.bind(this, this.updatePlaySlider), this.SLIDER_INTERVAL);
 		
-		if (window.PalmSystem) this.$.headsetService.call({});
+		if (window.PalmSystem) {
+			this.$.headsetService.call({subscribe: true});
+			this.$.headsetButtonService.call({subscribe: true});
+		}
 	},
 	
 	destroy: function() {
@@ -181,6 +184,24 @@ enyo.kind({
 			this.plays) this.togglePlay();
 	},
 	
+	headsetButtonPressed: function(sender, event) {
+		if (event.state == "down") {
+			switch (event.key) {
+				case "play":
+					if (this.episode && !this.plays) this.togglePlay();
+					break;
+				case "pause":
+				case "stop":
+					if (this.plays) this.togglePlay();
+					break;
+				case "next":
+					break;
+				case "prev":
+					break;
+			}
+		}
+	},
+		
 	isAtStartOfPlayback: function() {
 		return this.$.sound.audio.currentTime === 0;
 	},
