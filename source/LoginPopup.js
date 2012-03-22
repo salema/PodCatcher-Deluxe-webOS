a1a35440966fe57e8d9da62434f5d6b8125001ea
salema <kevin@alliknow.net>
2012-03-22 13:13:16 +0100

    Update app name

--- source/LoginPopup.js
+++ source/LoginPopup.js
@@ -1,18 +1,18 @@
 /** Copyright 2011, 2012 Kevin Hausmann
  *
- * This file is part of Yet Another Simple Pod Catcher.
+ * This file is part of Video PodCatcher Deluxe.
  *
- * Yet Another Simple Pod Catcher is free software: you can redistribute it 
+ * Video PodCatcher Deluxe is free software: you can redistribute it 
  * and/or modify it under the terms of the GNU General Public License as 
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * Yet Another Simple Pod Catcher is distributed in the hope that it will be useful,
+ * Video PodCatcher Deluxe is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Yet Another Simple Pod Catcher. If not, see <http://www.gnu.org/licenses/>.
+ * along with Video PodCatcher Deluxe. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
